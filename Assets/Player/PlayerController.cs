09100f33cba6e35f5f0086c4ed0e127e84d2ab72
Karl Zylinski <karl@zylinski.se>
2014-07-18 19:19:49 +0200

    Added player with placeholder graphics.

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Vector3 _velocity;
+
+        public void Start ()
+        {
+            _velocity = new Vector3(0, 0, 0);
+        }
+
+        public void Update ()
+        {
+            transform.position += _velocity;
+        }
+    }
+}
